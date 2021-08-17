9e5dab591e2647ef710d9dd63756edaa6e50e998
Jakub Plecháč <loxtox74@gmail.com>
2021-08-17 20:52:11 +0200

    frontend done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import logo from "./logo512.png";
 import "./App.css";
 
 import { Widget, addResponseMessage } from "react-chat-widget";
@@ -6,17 +7,29 @@ import "react-chat-widget/lib/styles.css";
 
 function App() {
   useEffect(() => {
-    addResponseMessage("Welcome to this awesome chat!");
+    for (let i = 0; i < 5; i++) {
+      addResponseMessage("test 1");
+    }
   }, []);
 
   const handleNewUserMessage = (newMessage) => {
     console.log(`New message incoming! ${newMessage}`);
+    console.log(newMessage);
     // Now send the message throught the backend API
   };
 
   return (
     <div className="App">
-      <Widget handleNewUserMessage={handleNewUserMessage} />
+      <Widget
+        title="Chat app"
+        subtitle="Chat with your buddies"
+        profileAvatar={logo}
+        handleNewUserMessage={handleNewUserMessage}
+        resizable={true}
+        fullScreenMode={false}
+        emojis={true}
+        showTimeStamp={false}
+      />
     </div>
   );
 }

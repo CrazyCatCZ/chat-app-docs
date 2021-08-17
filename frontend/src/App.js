b0c32e4e826d8eb2d07f53bada66e4737ad4f93d
Jakub Plecháč <loxtox74@gmail.com>
2021-08-17 18:49:49 +0200

    downgraded react-chat-widget package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
+import React, { useEffect } from "react";
 import "./App.css";
 
+import { Widget, addResponseMessage } from "react-chat-widget";
+import "react-chat-widget/lib/styles.css";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  useEffect(() => {
+    addResponseMessage("Welcome to this awesome chat!");
+  }, []);
+
+  const handleNewUserMessage = (newMessage) => {
+    console.log(`New message incoming! ${newMessage}`);
+    // Now send the message throught the backend API
+  };
+
+  return (
+    <div className="App">
+      <Widget handleNewUserMessage={handleNewUserMessage} />
+    </div>
+  );
 }
 
 export default App;

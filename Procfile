d98b08648dd624f216861e92813823b039209206
Jakub Plecháč <loxtox74@gmail.com>
2021-09-12 16:36:53 +0200

    edited asgi.py

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: python manage.py makemigrations && python manage.py migrate
 web: daphne backend.asgi:application --port $PORT --bind 0.0.0.0 -v2
+worker: python manage.py runworker channels --settings=backend.settings -v2
--- backend/asgi.py
+++ backend/asgi.py
@@ -1,9 +1,10 @@
-import os
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
 
+import os
 from channels.layers import get_channel_layer
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 import public_chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
@@ -12,7 +13,7 @@ import django
 django.setup()
 
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
+  "http": django_asgi_app,
   "websocket": AuthMiddlewareStack(
         URLRouter(
             public_chat.routing.websocket_urlpatterns,

8f7dfd7bbda2c83935db33cf63be4b5621042e79
Jakub Plecháč <loxtox74@gmail.com>
2021-09-12 22:36:50 +0200

    temporarily changed ws_scheme to ws://

--- public_chat/routing.py
+++ public_chat/routing.py
@@ -3,5 +3,6 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'wss/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
     #path('ws/chat/messages/', consumers.DRFConsumer.as_asgi()),
 ]
